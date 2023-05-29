feeaba4845125688cc668f9ffb5225727d7a48aa
FaisalM <122456185+Faisal-Algabni@users.noreply.github.com>
2023-05-29 13:42:06 +0300

    Update the code

--- pathPlanning.py
+++ pathPlanning.py
@@ -1,122 +1,157 @@
+# Define the initial and goal states
+import math
+import time
+
+start_state = (0, 0)  # Initial state represented as (x, y)
+goal_state = (9, 9)  # Goal state represented as (x, y)
+
+
+# Define the successor function
+def get_successors(state):
+    x, y = state
+    successors = []
+
+    # Generate all possible successors
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue  # Skip current state
+
+            new_x = x + dx
+            new_y = y + dy
+
+            # Check if the successor is within the grid boundaries
+            if 0 <= new_x < 10 and 0 <= new_y < 10:
+                successors.append((new_x, new_y))
+
+    return successors
+
+
+class Node:
+    def __init__(self, state, cost, heuristic):
+        self.state = state
+        self.cost = cost
+        self.heuristic = heuristic
+        self.total_cost = cost + heuristic
+        self.parent = None
+
+    def __lt__(self, other):
+        return self.total_cost < other.total_cost
+
+    def __eq__(self, other):
+        return self.total_cost == other.total_cost
+
+    def __gt__(self, other):
+        return self.total_cost > other.total_cost
 import heapq
-from math import sqrt
 
-ROW = 9
-COL = 10
+fringe = []  # Priority queue for the fringe
 
-class Cell:
-    def __init__(self):
-        self.parent_i = 0
-        self.parent_j = 0
-        self.f = 0
-        self.g = 0
-        self.h = 0
+def add_to_fringe(node):
+    heapq.heappush(fringe, (node.total_cost, node))
 
-def is_valid(row, col):
-    return 0 <= row < ROW and 0 <= col < COL
+def get_from_fringe():
+    _, node = heapq.heappop(fringe)
+    return node
 
-def is_unblocked(grid, row, col):
-    return grid[row][col] == 1
+def is_fringe_empty():
+    return len(fringe) == 0
 
-def is_destination(row, col, dest):
-    return row == dest[0] and col == dest[1]
+def heuristic(state, goal_state):
+    x1, y1 = state
+    x2, y2 = goal_state
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-def calculate_h_value(row, col, dest):
-    return sqrt((row - dest[0])**2 + (col - dest[1])**2)
+def get_cost(state, successor_state):
+    x1, y1 = state
+    x2, y2 = successor_state
 
-def trace_path(cell_details, dest):
-    print("The Path is:")
-    row, col = dest
-    path = []
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
 
-    while not (cell_details[row][col].parent_i == row and cell_details[row][col].parent_j == col):
-        path.append((row, col))
-        row, col = cell_details[row][col].parent_i, cell_details[row][col].parent_j
+    if dx == 1 and dy == 1:
+        return math.sqrt(2)
+    else:
+        return 1
 
-    path.append((row, col))
+def A_star_search(start_state, goal_state):
+    # Create the start node
+    start_node = Node(start_state, 0, heuristic(start_state, goal_state))
+    add_to_fringe(start_node)
 
-    for p in reversed(path):
-        if p[0] == 2 or p[0] == 1:
-            print(f"-> ({p[0]}, {p[1] - 1})")
-        else:
-            print(f"-> ({p[0]}, {p[1]})")
+    while not is_fringe_empty():
+        current_node = get_from_fringe()
 
-def a_star_search(grid, src, dest):
-    if not is_valid(src[0], src[1]):
-        print("Source is invalid")
-        return
+        if current_node.state == goal_state:
+            # Goal reached, construct the path
+            path = []
+            while current_node:
+                path.append(current_node.state)
+                current_node = current_node.parent
+            path.reverse()
+            return path
 
-    if not is_valid(dest[0], dest[1]):
-        print("Destination is invalid")
-        return
+        successors = get_successors(current_node.state)
+        for successor_state in successors:
+            # Calculate the cost and heuristic for the successor
+            successor_cost = current_node.cost + get_cost(current_node.state, successor_state)
+            successor_heuristic = heuristic(successor_state, goal_state)
 
-    if not is_unblocked(grid, src[0], src[1]) or not is_unblocked(grid, dest[0], dest[1]):
-        print("Source or the destination is blocked")
-        return
+            # Create the successor node
+            successor_node = Node(successor_state, successor_cost, successor_heuristic)
+            successor_node.parent = current_node
 
-    if is_destination(src[0], src[1], dest):
-        print("We are already at the destination")
-        return
+            add_to_fringe(successor_node)
 
-    cell_details = [[Cell() for _ in range(COL)] for _ in range(ROW)]
+    # No path found
+    return None
+import random
 
-    i, j = src
-    cell_details[i][j].f = 0
-    cell_details[i][j].g = 0
-    cell_details[i][j].h = 0
-    cell_details[i][j].parent_i = i
-    cell_details[i][j].parent_j = j
+grid_size = 10
+obstacle_percentage = 10
 
-    open_list = []
+obstacle_count = int(grid_size * grid_size * obstacle_percentage / 100)
+obstacles = random.sample(range(grid_size * grid_size), obstacle_count)
+grid = [[1 if i * grid_size + j in obstacles else 0 for j in range(grid_size)] for i in range(grid_size)]
 
-    heapq.heappush(open_list, (0.0, (i, j)))
+# Call the A* search function
+path = A_star_search(start_state, goal_state)
+if path:
+    print("Path found:", path)
+else:
+    print("No path found.")
+problem_tests = 20
+obstacle_percentages = range(10, 100, 10)
 
-    found_dest = False
+results = []
 
-    while open_list:
-        current = heapq.heappop(open_list)
-        i, j = current[1]
+for obstacle_percentage in obstacle_percentages:
+    total_time = 0
+    worst_case = float('-inf')
+    best_case = float('inf')
 
-        cell_details[i][j].f = current[0]
+    for _ in range(problem_tests):
+        # Generate a random grid with obstacles
+        obstacle_count = int(grid_size * grid_size * obstacle_percentage / 100)
+        obstacles = random.sample(range(grid_size * grid_size), obstacle_count)
+        grid = [[1 if i * grid_size + j in obstacles else 0 for j in range(grid_size)] for i in range(grid_size)]
 
-        for k in range(-1, 2):
-            for l in range(-1, 2):
-                if is_valid(i + k, j + l):
-                    if is_destination(i + k, j + l, dest):
-                        cell_details[i + k][j + l].parent_i = i
-                        cell_details[i + k][j + l].parent_j = j
-                        trace_path(cell_details, dest)
-                        found_dest = True
-                        return
+        # Measure the execution time
+        start_time = time.time()
+        path = A_star_search(start_state, goal_state)
+        end_time = time.time()
 
-                    elif not cell_details[i + k][j + l].f and is_unblocked(grid, i + k, j + l):
-                        g_new = cell_details[i][j].g + 1
-                        h_new = calculate_h_value(i + k, j + l, dest)
-                        f_new = g_new + h_new
+        if path:
+            execution_time = end_time - start_time
+            total_time += execution_time
+            worst_case = max(worst_case, execution_time)
+            best_case = min(best_case, execution_time)
 
-                        if cell_details[i + k][j + l].f > f_new or cell_details[i + k][j + l].f == 0:
-                            heapq.heappush(open_list, (f_new, (i + k, j + l)))
-                            cell_details[i + k][j + l].f = f_new
-                            cell_details[i + k][j + l].g = g_new
-                            cell_details[i + k][j + l].h = h_new
-                            cell_details[i + k][j + l].parent_i = i
-                            cell_details[i + k][j + l].parent_j = j
+    average_time = total_time / problem_tests
+    results.append((obstacle_percentage, average_time, worst_case, best_case))
 
-    if not found_dest:
-        print("Destination not found")
+# Print the results as a table
+print("Obstacle %   |     Average Time     |     Worst Case     |    Best Case     |     Total cost     |     Path solution     |")
+for result in results:
+    print("{:<12} | {:<12} | {:<10} | {:<9}".format(*result) )
 
-    
-grid = [[ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        [ 1, 0, 0, 0, 0, 0, 0, 1, 1, 1 ],
-        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        [ 0, 0, 1, 0, 0, 0, 0, 0, 1, 1 ],
-        [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-        [ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 ],            
-        [ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 ]]
- 
-src = [8, 0]
- 
-dest = [0, 6]
-a_star_search(grid, src, dest)
